94dc3273c8fcc8b0fae74ece41758d7ec52caa63
unknown <ganeshgudiboina@gmail.com>
2024-12-31 12:37:29 +0530

    added main pages

--- auth-app/src/App.jsx
+++ auth-app/src/App.jsx
@@ -1,7 +1,5 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
-  return <h1>App</h1>;
-};
+const App = () => {};
 
 export default App;
