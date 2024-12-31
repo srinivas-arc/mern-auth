e5e508b91f89dbb3cc639015bfe69e46737254d1
unknown <ganeshgudiboina@gmail.com>
2024-12-31 16:17:07 +0530

    db connection along with user model and test api

--- auth-app-backend/api/index.js
+++ auth-app-backend/api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routers/user.router.js";
 
 const app = express();
-
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connection Established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
+app.use("/api/test", userRoute);
